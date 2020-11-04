82d61c8d30fbc08ac7809c4091ec281fe7fbdaef
Shalinee <shalineebhawnani90.com>
2020-11-04 17:24:06 +0530

    [Shalinee] refactor added exportES file

---
+++ exportES.py
@@ -0,0 +1,51 @@
+from datetime import datetime
+from elasticsearch import Elasticsearch
+
+def connect_elasticsearch():
+    es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
+    if es.ping():
+        print('Connected')
+    else:
+        print('could not connect!')
+    return es
+def create_index(es_object, index_name):
+    created = False
+    # index settings
+    settings = {
+        "settings": {
+            "number_of_shards": 1,
+            "number_of_replicas": 0
+        },
+        "mappings": {
+            "salads": {
+                "dynamic": "strict",
+                "properties": {
+                    "title": {
+                        "type": "text"
+                    },
+                    "submitter": {
+                        "type": "text"
+                    },
+                    "description": {
+                        "type": "text"
+                    },
+                    "calories": {
+                        "type": "integer"
+                    },
+                }
+            }
+        }
+    }
+
+    try:
+        if not es_object.indices.exists(index_name):
+            es_object.indices.create(index=index_name, ignore=400, body=settings)
+            print('Created Index')
+        created = True
+    except Exception as ex:
+        print(str(ex))
+    finally:
+        return created
+
+es = connect_elasticsearch()
+create_index(es,'recipes')
