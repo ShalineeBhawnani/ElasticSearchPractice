fa3ea61ad8413d4e91693e54f2ebeaaf816024de
Shalinee <shalineebhawnani90.com>
2020-11-05 10:54:24 +0530

    [Shalinee] refactor delete index

--- exportES.py
+++ exportES.py
@@ -8,18 +8,26 @@ def connect_elasticsearch():
     else:
         print('could not connect!')
     return es
+
 def create_index(es_object, index_name):
     created = False
     try:
         if not es_object.indices.exists(index_name):
             es_object.indices.create(index=index_name)
             print('Created Index',index_name)
+        else :
+            print('Index Already Exist',index_name)
         created = True
     except Exception as ex:
         print("exception",str(ex))
     finally:
-        print("already there")
         return created
+
+def delete_index(es_object, index_name):
+    try:
+        if not es_object.indices.exists(index_name):
+            es_object.indices.delete(index=index_name)
+            print('Deleted Index',index_name)
 
 es = connect_elasticsearch()
 create_index(es,'recipes')
