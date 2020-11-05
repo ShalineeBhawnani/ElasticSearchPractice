6ef89ee0e457ef202d541155a646a8442dc4d2aa
Shalinee <shalineebhawnani90.com>
2020-11-05 14:45:46 +0530

    [Shalinee] refactor search_by_index_and_id

--- exportES.py
+++ exportES.py
@@ -1,6 +1,14 @@
 from datetime import datetime
 from elasticsearch import Elasticsearch
 
+data = {
+        "author": "Chestermo",
+        "gender": "male",
+        "age": "24",
+        "body_fat": "15%",
+        "interest": ["couch potato", "eat and sleep"]
+    }
+
 def connect_elasticsearch():
     es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
     if es.ping():
@@ -24,10 +32,19 @@ def create_index(es_object, index_name):
     finally:
         return created
 
+def insert_one_data(_index, data):
+    res = es.index(index=_index, doc_type='authors', id=1, body=data)
+    # index will return insert info: like as created is True or False
+    print(res)
+
+def insert_data_by_bulk(data):
+    res = helpers.bulk(es, data)
+    print(res)
+
 # Delete an Elasticsearch index
 def delete_index(es_object, index_name):
     try:
-        if not es_object.indices.exists(index_name):
+        if es_object.indices.exists(index_name):
             es_object.indices.delete(index=index_name)
             print('Deleted Index',index_name)
     except Exception as ex:
@@ -40,8 +57,19 @@ def get_index(es_object):
 
     print(indices_dict.items())
     print("get index names only in cluster",indices_dicts)
-    
+
+def search_by_index_and_id(es,_index, _id):
+    res = es.get(
+        index=_index,
+        id=_id
+    )
+    print(res)
+    return res
+
 es = connect_elasticsearch()
-# create_index(es,'recipes')
-get_index(es)
+# create_index(es,'author')
+# insert_one_data('author', data)
+search_by_index_and_id(es,'author', 1)
+
+# get_index(es)
 
