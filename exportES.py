3c1a303ff7602c77cd6b2abcc95057230a4e4017
Shalinee <shalineebhawnani90.com>
2020-11-05 11:24:31 +0530

    [Shalinee] refactor get_index

--- exportES.py
+++ exportES.py
@@ -9,6 +9,7 @@ def connect_elasticsearch():
         print('could not connect!')
     return es
 
+# Create an Elasticsearch index
 def create_index(es_object, index_name):
     created = False
     try:
@@ -23,12 +24,24 @@ def create_index(es_object, index_name):
     finally:
         return created
 
+# Delete an Elasticsearch index
 def delete_index(es_object, index_name):
     try:
         if not es_object.indices.exists(index_name):
             es_object.indices.delete(index=index_name)
             print('Deleted Index',index_name)
+    except Exception as ex:
+        print("exception",str(ex))
 
+# Get every indices class in a cluster
+def get_index(es_object):
+    indices_dict = es_object.indices.get_alias("*")
+    indices_dicts = es_object.indices.get_alias().keys()
+
+    print(indices_dict.items())
+    print("get index names only in cluster",indices_dicts)
+    
 es = connect_elasticsearch()
-create_index(es,'recipes')
+# create_index(es,'recipes')
+get_index(es)
 
