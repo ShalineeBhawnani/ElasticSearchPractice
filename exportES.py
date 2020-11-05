1fc539a15d6b81f26788e1a5d656440828af04c5
Shalinee <shalineebhawnani90.com>
2020-11-05 10:50:25 +0530

    [Shalinee] refactor exportES

--- exportES.py
+++ exportES.py
@@ -10,42 +10,17 @@ def connect_elasticsearch():
     return es
 def create_index(es_object, index_name):
     created = False
-    # index settings
-    settings = {
-        "settings": {
-            "number_of_shards": 1,
-            "number_of_replicas": 0
-        },
-        "mappings": {
-            "salads": {
-                "dynamic": "strict",
-                "properties": {
-                    "title": {
-                        "type": "text"
-                    },
-                    "submitter": {
-                        "type": "text"
-                    },
-                    "description": {
-                        "type": "text"
-                    },
-                    "calories": {
-                        "type": "integer"
-                    },
-                }
-            }
-        }
-    }
-
     try:
         if not es_object.indices.exists(index_name):
-            es_object.indices.create(index=index_name, ignore=400, body=settings)
-            print('Created Index')
+            es_object.indices.create(index=index_name)
+            print('Created Index',index_name)
         created = True
     except Exception as ex:
-        print(str(ex))
+        print("exception",str(ex))
     finally:
+        print("already there")
         return created
 
 es = connect_elasticsearch()
 create_index(es,'recipes')
+
